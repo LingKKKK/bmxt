e721eb26b442f69c9c1c9b77174190790a835b42
kite17 <wangkunpeng@kenrobot.com>
2017-06-20 15:00:40 +0800

    bug:fix

--- app/helpers.php
+++ app/helpers.php
@@ -68,7 +68,7 @@ if (! function_exists('is_mobile')) {
         if (! is_numeric($mobile)) {
             return false;
         }
-        $regexMobile = '/^13[\d]{9}$|^14[5,7]{1}\d{8}$|^15[^4]{1}\d{8}$|^17[0,3,6,7,8]{1}\d{8}$|^18[\d]{9}$/';
+        $regexMobile = '/^13[\d]{9}$|^14[5,7]{1}\d{8}$|^15[^4]{1}\d{8}$|^17\d{9}$|^18[\d]{9}$/';
         return preg_match($regexMobile, $mobile) ? true : false;
     }
 }
