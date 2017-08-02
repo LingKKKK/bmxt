e5dc8a11423c1daa1a955a214918d16e7e734988
kite17 <wangkunpeng@kenrobot.com>
2017-08-02 17:36:44 +0800

    修改数据库字段

--- database/migrations/2017_07_18_134918_create_matchbj_data_table.php
+++ database/migrations/2017_07_18_134918_create_matchbj_data_table.php
@@ -25,7 +25,7 @@ class CreateMatchbjDataTable extends Migration
             $table->increments('id');
             $table->integer('event_group')->comments('赛项分组');
             $table->integer('parent_id')->comments('上级类别ID');
-            $table->string('name', 50)->comments('名称');
+            $table->string('name', 300)->comments('名称');
             $table->string('description', 1500)->comments('描述');
         });
 
@@ -43,17 +43,17 @@ class CreateMatchbjDataTable extends Migration
             // 联系人
             $table->string('contact_name', 50)->comments('联系人姓名');
             $table->string('contact_mobile', 50)->comments('联系电话');
-            $table->string('contact_email', 50)->comments('联系人邮箱');
-            $table->string('contact_remark', 50)->comments('备注');
+            $table->string('contact_email', 100)->comments('联系人邮箱');
+            $table->string('contact_remark', 1500)->comments('备注');
 
             // 发票
-            $table->string('invoice_title', 50)->comments('开票抬头');
-            $table->string('invoice_code', 50)->comments('统一社会信用代码');
-            $table->string('invoice_money', 50)->comments('开票金额（单位：元）');
-            $table->string('invoice_type', 50)->comments('发票明细');
-            $table->string('invoice_mail_address', 100)->comments('收件地址');
-            $table->string('invoice_mail_recipients', 20)->comments('收件人姓名');
-            $table->string('invoice_mail_mobile', 30)->comments('收件人联系电话');
+            $table->string('invoice_title', 500)->comments('开票抬头');
+            $table->string('invoice_code', 100)->comments('统一社会信用代码');
+            $table->string('invoice_money', 100)->comments('开票金额（单位：元）');
+            $table->string('invoice_type', 100)->comments('发票明细');
+            $table->string('invoice_mail_address', 1000)->comments('收件地址');
+            $table->string('invoice_mail_recipients', 50)->comments('收件人姓名');
+            $table->string('invoice_mail_mobile', 50)->comments('收件人联系电话');
             $table->string('invoice_mail_email', 100)->comments('收件人邮箱');
             $table->string('invoice_remark', 1500)->comments('发票邮寄备注');
 
@@ -72,28 +72,29 @@ class CreateMatchbjDataTable extends Migration
             $table->string('type', 20)->comments('成员类型: 领队,指导教师,队长,队员');
 
             // 基本信息
-            $table->string('name', 50)->comments('名字');
-            $table->string('mobile', 30)->comments('手机号码');
-            $table->string('email', 100)->comments('邮箱');
+            $table->string('name', 100)->comments('名字');
+            $table->string('mobile', 50)->comments('手机号码');
+            $table->string('email', 300)->comments('邮箱');
             $table->string('idcard_type', 50)->comments('证件类型: 身份证,护照');
-            $table->string('idcard_no', 50)->comments('证件号码:');
-            $table->string('nation', 30)->comments('民族');
-            $table->tinyInteger('sex')->comments('性别');
-            $table->string('birthday', 20)->comments('生日');         // 年龄生日二选一，默认生日
+            $table->string('idcard_no', 300)->comments('证件号码:');
+            $table->string('nation', 100)->comments('民族');
+            $table->string('sex', 30)->comments('性别');
+            $table->string('birthday', 100)->comments('生日');         // 年龄生日二选一，默认生日
             $table->tinyInteger('age')->unsigned()->comments('年龄'); // 可选
             $table->mediumInteger('height')->comments('身高:cm');
-            $table->string('photo_url', 50)->comments('照片地址');
+            $table->string('photo_url', 1000)->comments('照片地址');
 
             //其他资料
-            $table->string('vocation', 50)->comments('职业');
-            $table->string('work_unit', 50)->comments('工作单位');
-            $table->string('register_address', 50)->comments('户籍地址');
-            $table->string('home_address', 50)->comments('现居详情');
+            $table->string('vocation', 300)->comments('职业');
+            $table->string('work_unit', 300)->comments('工作单位');
+            $table->string('register_address', 300)->comments('户籍地址');
+            $table->string('home_address', 300)->comments('现居详情');
 
             $table->string('remark', 1500)->comments('备注');
             $table->string('profiles', 2000)->comments('其他资料');
 
             $table->timestamps();
+            $table->softDeletes();
 
             // $table->foreign('team_id')->references('id')->on('competition_teams');
         });
