b6c440b0b13f2fea844965eaeaf6efb553bf2241
Lingkkkkkk <yuepengfei@kenrobot.com>
2018-03-14 14:03:58 +0800

    bug:fixed

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,85 +1,185 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <meta http-equiv="X-UA-Compatible" content="IE=Edge,chrome=1">
-    <title>RoboCom国际公开赛－－青少年人工智能编成挑战赛</title>
-    <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/matchbj.css')}}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/kenrobot.css')}}">
-    <link href="https://cdn.bootcss.com/datepicker/0.5.3/datepicker.css" rel="stylesheet">
+    <title></title>
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/register.css')}}">
 </head>
 <body>
-    <div class="main">
-        <div class="header">
-            <div class="inner">
-                <div class="banner-img"></div>
-            </div>
-        </div>
-        <div class="instructions clearfix active">
-            <h1 class="instructions-h">结束报名通知</h1>
-            <span style="font-family: PingFangSC-Regular;font-size: 14px;color: #666666;letter-spacing: 1px;line-height: 24px;text-align: justify;margin-bottom: 10px;color: black;">各位参赛选手与老师：</br>
-                <b style="text-align: left; text-indent:2em; font-weight: normal; display: block;margin-top: 20px;color: black;width: 930px;">2018年RoboCom国际公开赛——青少年人工智能编程挑战赛在线报名工作从11月30日开始收到国内广大青少年选手的高度关注。公开报名已于12月15日结束，不再接受公开线上报名。如有疑问可以联系各相关负责人。</b></br>
-                特此通知！</br>
-            </span>
-            <div class="log" style="width: 380px;height: 200px; float: right;transform: translate(0px, -50px);position: relative;">
-                <img style="width: 100%;" src="{{asset('assets/img/log.png')}}" alt="">
-                <span style="position: absolute;right: 100px;top: 90px;position: absolute;text-align: right;color: black;font-family: PingFangSC-Regular;font-size: 14px;letter-spacing: 1px;line-height: 24px;display: block;margin-bottom: 10px;">RoboCom国际公开赛组委会<br>2017年12月15日</span>
-            </div>
-            <div class="clearfix"></div>
-            <h2 class="" style="margin-bottom: 20px; margin-top: 15px;">附一：负责人联系方式</h2>
-            <span class="instructions-span" style="padding-left: 20px;">1. 相关赛事项目技术咨询及比赛规则解释负责人：</span>
-            <span class="instructions-span" style="padding-left: 46px;"> a)图形化编程软件创意设计挑战赛  联系人：周善斌 电话：18576690069  樊军伟 电话：18588238651  贺晓山 电话：15994743223</span>
-            <span class="instructions-span" style="padding-left: 46px;"> b)智造大挑战赛 联系人：解俊杰　电话：18603015057</span>
-            <span class="instructions-span" style="padding-left: 46px;"> c)中鸣超级轨迹赛 联系人：王小君　电话：13500034449</span>
-            <span class="instructions-span" style="padding-left: 46px;"> d)单片机迷宫任务挑战赛 联系人：张健 电话：13811159341</span>
-            <span class="instructions-span" style="padding-left: 46px;"> e)RoboCom星际迷航  联系人：钟毅  电话：18978985303</span>
-            <span class="instructions-span" style="padding-left: 20px;">2. 组委会办公室</span>
-            <span class="instructions-span" style="padding-left: 46px;">李佳：13552892879  洪婕：13466616992 闫旭晖：13810882832</span>
-            <span class="instructions-span" style="padding-left: 46px;">座机：010-81055310</span>
-            <h2 style="margin-bottom: 20px; margin-top: 15px;">附二</h2>
-            <span class="instructions-span" style="padding-left: 20px; color: red;">交费流程如下：</span>
-            <span class="instructions-span" style="padding-left: 20px;">在线报名完成后，请根据自己所报赛项联系相应的项目参赛联系人缴费（请保留在线报名的队伍编码和转账成功的截图以方便核实）。</span>
-            <span class="instructions-span" style="padding-left: 20px;">1. 核实参赛队伍是否报名成功。并核实参赛人数。</span>
-            <span class="instructions-span" style="padding-left: 20px;">2. 支付参赛注册费</span>
-            <span class="instructions-span" style="padding-left: 20px;">付款对公账户：</span>
-            <span class="instructions-span" style="padding-left: 46px;">开户名称：北京搜获科技有限公司</span>
-            <span class="instructions-span" style="padding-left: 46px;">开户银行：中国工商银行北京分行东升路支行</span>
-            <span class="instructions-span" style="padding-left: 46px;">开户行号（大额支付代码）：102100000626</span>
-            <span class="instructions-span" style="padding-left: 46px;">银行账号：0200006209200036460</span>
-
-            <div class="clear"></div>
-        </div>
-        <div class="footer">
-            <div class="inner">
-                <div class="logo-all">
-                    <img src="{{ asset('assets/img/LOGO2.png')}}" alt="">
-                    <img src="{{ asset('assets/img/logo-white-word.png')}}" alt="">
-                </div>
-                <span class="sp1">© 2017 RoboCom 国际公开赛组委会  |  鄂ICP备16011249号-2 </span>
-                <span class="sp2">技术支持: 北京啃萝卜信息技术有限公司</span>
-            </div>
-        </div>
-        <div class="verificationcode_box">
-            <div class="showBox">
-                <span class="tip">提示: 请您在右侧输入图中的数字或者字母~</span>
-                <span id="tipes">验证成功之后,我们会将验证码发送至您的手机:  <i></i></span>
-                <span class="tipes-false">您输入的验证码有误,请核对后重新输入!!!</span>
-                <img id="captcha_img1" src="{{url('/captcha')}}">
-                <input id="v_code" type="text" placeholder="请输入">
-                <a id="sendCode" class="yes">确认</a>
-                <a class="no"><i class="icon kenrobot ken-close"></i></a>
-            </div>
-        </div>
-        <div class="codeError">
-            <div class="showBox">
-                <i class="icon kenrobot ken-close close"></i>
-                <div class="clear"></div>
-                <i class="icon kenrobot ken-close"></i>
-                <p>您输入的手机验证码有误,请核对短信后再次输入~</p>
+    <div class="header">
+        <div class="inner">
+            <img src="{{ asset('assets/img/banner_bg.png')}}" alt="">
+            <div class="logout">
+                <span></span>
             </div>
         </div>
     </div>
-    <script type="text/javascript" src="{{ asset('assets/js/jquery.min.js')}}"></script>
-    <script type="text/javascript" src="{{ asset('assets/js/jsrender.js')}}"></script>
-    <script type="text/javascript" src="{{ asset('assets/js/helpers.js')}}"></script>
+    <div class="content">
+        <div class="leader-info-tips">
+            @if(count($errors) > 0)
+                <span>{{$errors->first()}}</span>
+            @endif
+        </div>
+        <div class="dialog-user">
+            <a href="/login">登录</a>
+            <a>注册</a>
+        </div>
+        <form id="form" action="/register"  method="POST" novalidate>
+            <div class="inner clearfix">
+                <span class="tips">tips: 请输入您的邮箱、手机号进行注册</span>
+                <span class="tips-info" ></span>
+
+                <div class="input-field">
+                    <span class="input-label">邮箱  :</span>
+                    <input data-type="" class="input-field-text"  id="email" type="text" name="email" value="{{old('email')}}">
+                </div>
+                <div class="input-field">
+                    <span class="input-label">手机号  :</span>
+                    <input data-type="mobile" class="input-field-text"  id="mobile" type="text" name="mobile" value="{{old('mobile')}}">
+                </div>
+                <div class="input-field">
+                    <span class="input-label">昵称  :</span>
+                    <input data-type="username" class="input-field-text"  id="username" type="text" name="username" value="{{old('username')}}">
+                </div>
+                <div class="input-field">
+                    <span class="input-label">密码  :</span>
+                    <input data-type="password" class="input-field-text"  id="password" type="password" name="password" value="{{old('password')}}">
+                </div>
+                <div class="input-field">
+                    <span class="input-label">确认密码  :</span>
+                    <input data-type="password_confirmed" class="input-field-text"  id="password_confirmed" type="password" name="password_confirmed" value="{{old('password_confirmed')}}">
+                </div>
+                <div id="code" class="clearfix">
+                    <span class="input-label">验证码  :</span>
+                    <input name="verificationcode" id="verificationcode" class="code" type="text">
+                </div>
+                <a id="tel" class="tel">获取手机验证码</a>
+                <button class="submit-search" id="submit-search">注册</button>
+                <!-- <p class="button-style">查询</p> -->
+            </div>
+        </form>
+    </div>
+
+    <div class="identifying">
+        <div class="showBox">
+            <span class="tip">提示: 请您在右侧输入图中的数字或者字母~</span>
+            <span id="tipes">验证成功之后,我们会将验证码发送至您的手机:  <i></i></span>
+            <span class="tipes-false">您输入的手机号码或者验证码有误,请重新输入!!!</span>
+            <img src="{{url('/captcha')}}">
+            <input id="v_code" type="text" placeholder="请输入">
+            <a id="sendCode" class="yes">确认</a>
+            <a class="no">X</a>
+        </div>
+    </div>
+
+    <div class="footer">
+        <div class="inner">
+            <div class="logo-all">
+                <img src="{{ asset('assets/img/LOGO2.png')}}" alt="">
+                <img src="{{ asset('assets/img/logo-white-word.png')}}" alt="">
+            </div>
+            <span class="sp1">© 2017 RoboCom 国际公开赛组委会  |  鄂ICP备16011249号-2 </span>
+            <span class="sp2">技术支持: 北京啃萝卜信息技术有限公司</span>
+        </div>
+    </div>
+<script type="text/javascript" src="{{ asset('assets/js/jquery.min.js')}}"></script>
+<script type="text/javascript">
+    (function($){
+        $.fn.refreshCaptcha = function(){
+            if($(this).prop('tagName') == 'IMG'){
+                var timestamp = Date.parse(new Date());
+                $(this).attr('src', "{{url('/captcha')}}"+"?t="+timestamp);
+            }
+        }
+    })(jQuery);
+    //修改
+    function refresh_captcha($el) {
+        var timestamp = Date.parse(new Date());
+        $($el).attr('src', "{{url('/captcha')}}"+"?t="+timestamp);
+    }
+
+    function countdown() {
+        var t = 60;
+        var countdown = setInterval(function(){
+            $('#tel').html('重新获取验证码('+ t-- + ')s');
+            $('#tel').addClass('active');
+            if (t <= 0) {
+                $('#tel').html('获取验证码');
+                $('#tel').removeClass('active');
+                clearInterval(countdown);
+            }
+        },1000);
+    }
+
+    $(function(){
+        // 默认添加一次队员列表
+        setTimeout(function (){
+            $('#append_rank_new').click();
+        }, 1000)
+
+        // 点击刷新验证码图片
+        $('.identifying .showBox img').click(function (){
+            $(this).refreshCaptcha();
+        });
+
+        // 点击取消输入验证码
+        $('.identifying .no').click(function() {
+            // clearInterval(countdown);
+            $('.identifying').removeClass('active');
+        });
+
+        $('#v_code').click(function(event) {
+            // console.log(1)
+            $('.tipes-false').css('opacity', 0);
+        });
+
+        // 点击确认输入验证码
+        $('.identifying .yes').click(function() {
+            var captchacode = $('#v_code').val();
+            var mobile = $('#mobile').val();
+            var ID = $('#leader_id').val();
+            var type = 'mobile';
+            //console.log(captchacode,mobile,type);
+            $.post(
+                "{{url('/verificationcode/send')}}",
+                {
+                    type    : type,
+                    captcha : captchacode,
+                    mobile  : mobile,
+                },
+                function(res){
+                    if (res.status == 0) {
+                        // console.log('验证码填写成功并确定')
+                        refresh_captcha();
+                        $('.identifying').removeClass('active');
+                        // $('.submit-search').removeClass('active');
+                    } else {
+                        // console.log('验证码填写错误')
+                        $('.tipes-false').css('opacity', 1);
+                    }
+                }
+            );
+        });
+    })
+
+    // 发送手机验证码
+    $('#tel').click(function() {
+        var partten = /^1\d{10}$/;
+        if(partten.test($('#mobile').val())){
+           $('.identifying').addClass('active');
+           $('#tipes i').html($('#mobile').val());
+           countdown();
+        }else {
+            $('.tips-info').text('您输入的手机格式错误');
+            setTimeout(function (){
+                $('.tips-info').text('');
+            }, 3000)
+        }
+    });
+
+    // 点击input 提示信息清空
+    $('.input-field-text').focus(function(event) {
+        $('.leader-info-tips').css('opacity', 0)
+    });
+</script>
 </body>
 </html>
